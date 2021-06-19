7a5ae76ff9ab96a9da4a89bd079d71ef70c1c2b5
jill <github@phenom.studio>
2021-06-18 19:01:50 -0700

    initial commit

---
+++ src/lib/Intro/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    import BackgroundGradient from '$lib/BackgroundGradient.svelte';
+import BackGroundGradient from '$lib/BackgroundGradient.svelte';
+</script>
+
+<section class="fullscreen">
+    <BackgroundGradient />
+    <div class="center">
+        <div class="headline-wrapper">
+            <h2>portfolio of</h2>
+            <h1>Jill Fantauzza</h1>
+            <h3>people&ndash;centered experience designer & developer</h3>
+        </div>
+    </div>
+</section>
+
+<style>
+    h3 {
+        /* text-align: right; */
+        margin-left: 2.5rem;
+    }
+</style>
