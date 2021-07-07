23046c21eb523ae8c1947f08a317991f5c6316c1
jill <github@phenom.studio>
2021-07-06 21:05:12 -0400

    adjusted panel elements

---
+++ src/lib/PanelFullText/index.svelte
@@ -0,0 +1,18 @@
+<script>
+    import BackgroundGradient from '$lib/BackgroundGradient.svelte';
+
+    export let project;
+</script>
+
+<section 
+    id={project.id}
+    class="fullscreen"
+    class:panelColor="{project.panelColor === true}"
+>
+    <BackgroundGradient />
+    <div class="fullscreen center">
+        <div class="content smalltext">
+            {@html project.text} 
+       </div>
+    </div>
+</section>
---
+++ src/lib/PanelHeroVideo/index.svelte
@@ -0,0 +1,55 @@
+<script>
+	import NavBottom from '$lib/NavBottom.svelte';
+
+    export let project;
+    let {format, id, title, event, poster, src, nextSection} = project;
+</script>
+
+<section 
+    id={id}
+    class="panel center"
+    class:panelColor="{project.panelColor === true}"
+>
+
+    <video class="hero-video" style="background-image: url({ poster })"
+        { poster }
+        { src }
+		autoplay
+		muted
+		loop>
+        <track kind="captions">
+    </video>
+
+	<div class="headline bleed-padding">
+        {#if (title)}
+            <h2>{@html title}</h2>
+        {/if}
+        {#if (event)}
+            <h3>{@html event}</h3>
+        {/if}
+    </div>
+	<div class="nav-container">
+		<NavBottom { nextSection } />
+	</div>
+
+</section>
+
+<style>
+	video {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		object-position: 50% 50%;
+	}
+
+	.hero-video {
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: 50% 50%;
+	}
+
+	.nav-container {
+		position: absolute;
+		bottom: 2rem;
+	}
+</style>
