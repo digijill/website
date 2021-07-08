e6eef4b8e545576d8c1889ffcb42c90b916ab6a7
jill <github@phenom.studio>
2021-07-08 18:42:15 -0400

    incorporated adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,18 @@
 /** @type {import('@sveltejs/kit').Config} */
+
+import adapter from '@sveltejs/adapter-static';
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({
+			// default options
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		trailingSlash: "ignore"
 	}
 };
 
